298c31cb83309e174f4482930e91559ce4ec89b6
Kevin Phillips <pya2626@gmail.com>
2019-04-19 09:08:19 -0400

    add player stats total file

---
+++ player_stats_totals.php
@@ -0,0 +1,45 @@
+<?php
+
+// Get cURL resource
+$ch = curl_init();
+
+// Set url
+//curl_setopt($ch, CURLOPT_URL, "https://api.mysportsfeeds.com/v2.1/pull/mlb/2019-regular/player_stats_totals.json?limit=1");
+curl_setopt($ch, CURLOPT_URL, "https://api.mysportsfeeds.com/v2.1/pull/mlb/current_season.json");
+
+// Set method
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+
+// Set options
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// Set compression
+curl_setopt($ch, CURLOPT_ENCODING, "gzip");
+
+// Set headers
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+	"Authorization: Basic " . base64_encode("3e610f3c-19bb-400c-b0b6-887575" . ":" . "MYSPORTSFEEDS")
+]);
+
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+// Send the request & save response to $resp
+$resp = curl_exec($ch);
+$response = json_decode($resp);
+
+/*
+if (!$resp) {
+	die('Error: "' . curl_error($ch) . '" - Code: ' . curl_errno($ch));
+} else {
+	echo "Response HTTP Status Code : " . curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	echo "\nResponse HTTP Body : " . $resp;
+}
+*/
+print"<pre>";
+print_r($response);
+print"</pre>";
+
+// Close request to clear up some resources
+curl_close($ch);
+
+?>
